3694a5f58e606ee383178fd345db0a5d2fef35d9
daniel <seahagos@gmail.com>
2018-08-20 14:58:40 -0500

    last update

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import MainPage from "./MainPage";
+import "./Companies.css";
+export default function Footer() {
+  return (
+    <div class="footer">
+      {<p className="foott">Daniel Hagos NoDb project 2018 </p>}
+    </div>
+  );
+}
